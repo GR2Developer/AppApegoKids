3bdf8bb362927ad1e7e71a0d67544b343a6db3d6
Gabriel Moraes <42454894+GuardaCostas@users.noreply.github.com>
2018-11-05 15:08:23 -0200

    05/11/2018

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  Loading,
+  LoadingController,
+  AlertController
+} from 'ionic-angular';
 
-import { DatabaseProvider } from '../../providers/database/database';
+
+
 import { Storage } from '@ionic/storage';
 import Firebase from 'firebase';
 import { AuthProvider } from '../../providers/auth/auth';
+import { DatabaseProvider } from '../../providers/database/database';
+import { ImageLoader } from 'ionic-image-loader';
+
 
 /**
  * Generated class for the UserProfilePage page.
@@ -19,17 +30,26 @@ import { AuthProvider } from '../../providers/auth/auth';
   templateUrl: 'user-profile.html',
 })
 export class UserProfilePage {
+  private database: Firebase.firestore.Firestore;
+  private unsub: any;
+
+  public myProducts: any[];
+  public myIndex: any; //index da tabela html para exclusão correta dos itens
 
   public userName: any;
+  private productsCollection: string = "Products";
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private databaseProvider: DatabaseProvider,
     private loadingCtrl: LoadingController,
+    public alertController: AlertController,
     //private authProvider: AuthProvider,
     public storage: Storage
   ) {
+    this.database = Firebase.firestore();
+    this.addListener();
 
 
     //console.log("uid: "+Firebase.auth().currentUser.uid)
@@ -65,4 +85,69 @@ export class UserProfilePage {
     this.navCtrl.push("UserProductsListPage");
   }
 
+  addListener(): any {
+    const uid = Firebase.auth().currentUser.uid;
+    this.unsub = this.database.collection(this.productsCollection).where('uid', '==', uid).onSnapshot(querySnapshot => {
+      this.myProducts = [];
+      querySnapshot.forEach(doc => {
+        console.log("doc database");
+        console.dir(doc.data());
+        this.myProducts.push({
+          docId: doc.id,
+          name: doc.data().name,
+          description: doc.data().description,
+          price: doc.data().price,
+          category: doc.data().category,
+          subcategory: doc.data().subcategory,
+          imgUrl: doc.data().imgUrl,
+          imgPath: doc.data().imgPath
+        });
+      });
+    })
+  }
+
+  
+  addDocument() {
+    this.navCtrl.push("ManageProductPage", { isEdited: false, collection: this.productsCollection });
+  }
+
+  updateDocument(obj): void {
+    let params: any = {
+      collection: this.productsCollection,
+      product: obj
+    };
+    this.navCtrl.push('ManageProductPage', { record: params, isEdited: true });
+  }
+
+
+  
+  deleteDocument(obj): void {
+    //Adiciona um loading na tela para bloquear interação do usuário
+    const loading: Loading = this.loadingCtrl.create();
+    loading.present();
+    this.databaseProvider.deleteImageInStorage(obj.imgPath).then(() => {
+      this.databaseProvider.deleteDocument(this.productsCollection,
+        obj.docId)
+        .then(() => {
+          loading.dismissAll();
+          this.displayAlert('Feito!', 'O produto ' + obj.name + ' foi removido com sucesso.');
+        })
+        .catch((error: any) => {
+          loading.dismissAll();
+          this.displayAlert('Erro', error.message);
+        });
+    });
+  }
+
+  displayAlert(title: string,
+    message: string): void {
+    let alert: any = this.alertController.create({
+      title: title,
+      subTitle: message,
+      buttons: [{
+        text: 'OK!'
+      }]
+    });
+    alert.present();
+  }
 }
